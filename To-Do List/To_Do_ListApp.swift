899fb7e0751b7cdecac1397fbdbe3e8962a50149
Briana-Koo <RoyalBlueSnow@gmail.com>
2022-01-11 10:48:30 -0600

    Created custom ToDoList class

--- To-Do List/To_Do_ListApp.swift
+++ To-Do List/To_Do_ListApp.swift
@@ -15,3 +15,9 @@ struct To_Do_ListApp: App {
         }
     }
 }
+
+class ToDoList: ObservableObject {
+    @Published var items = [ToDoItem(priority: "High", description: "Take out trash", dueDate: Date()),
+                            ToDoItem(priority: "Medium", description: "Pick up clothes", dueDate: Date()),
+                            ToDoItem(priority: "Low", description: "Eat a donut", dueDate: Date())]
+ }
---
+++ ToDoList.swift
@@ -0,0 +1,8 @@
+//
+//  ToDoList.swift
+//  To-Do List
+//
+//  Created by Student on 1/11/22.
+//
+
+import Foundation
